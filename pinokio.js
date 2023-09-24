60459b1b49bc2e936c86170237db633558feb619
Raphael <67842535+Snowad14@users.noreply.github.com>
2023-09-24 20:22:53 +0200

    Add files via upload

---
+++ pinokio.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: "Rerender A Video"
+      description: "Rerender_A_Video: Zero-Shot Text-Guided Video-to-Video Translation",
+      icon: "icon.png",
+  menu: [{
+    html: "<i class='fa-solid fa-plug'></i> Install",
+    type: "link",
+    href: "install.json?run=true&fullscreen=true"
+  },
+  {
+    html: "<i class='fa-solid fa-power-off'></i> Start",
+    type: "link",
+    href: "start.json?run=true&fullscreen=true"
+  },
+  {
+    html: "<i class='fa-solid fa-gear'></i> Config",
+    type: "link",
+    href: "config.json?run=true&fullscreen=true"
+  }]
+}
